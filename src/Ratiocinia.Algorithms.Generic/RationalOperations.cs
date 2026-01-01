91875528817574b4fada42eee20351b8a2b5b1d9
Viktor Tselov <qbit86@gmail.com>
2026-01-01 02:54:41 +0200

    [Algorithms] Add RationalOperations class with Multiply method for rational number operations

---
+++ src/Ratiocinia.Algorithms.Generic/RationalOperations.cs
@@ -0,0 +1,19 @@
+﻿namespace Ratiocinia.Algorithms.Generic
+{
+    public static class RationalOperations
+    {
+        public static (T Numerator, T Denominator) Multiply<T, TPolicy>(
+            T leftNumerator, T leftDenominator, T rightNumerator, T rightDenominator, TPolicy policy)
+            where TPolicy : IDivisionFunctions<T>, IGreatestCommonDivisorFunctions<T>, IMultiplyFunctions<T>
+        {
+            // https://github.com/boostorg/rational/blob/boost-1.90.0/include/boost/rational.hpp#L571
+            var gcd1 = policy.Gcd(leftNumerator, rightDenominator);
+            var gcd2 = policy.Gcd(leftDenominator, rightNumerator);
+            var numerator = policy.Multiply(policy.Divide(leftNumerator, gcd1), policy.Divide(rightNumerator, gcd2));
+            var denominator =
+                policy.Multiply(policy.Divide(leftDenominator, gcd2), policy.Divide(rightDenominator, gcd1));
+
+            return (numerator, denominator);
+        }
+    }
+}
