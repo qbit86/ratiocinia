d6e6e48359c7a60b6deb1dd1e874414be368bddb
Viktor Tselov <qbit86@gmail.com>
2026-01-01 03:24:39 +0200

    [Algorithms] Implement Add method in RationalOperations for rational number addition

--- src/Ratiocinia.Algorithms.Generic/RationalOperations.cs
+++ src/Ratiocinia.Algorithms.Generic/RationalOperations.cs
@@ -2,6 +2,23 @@
 {
     public static class RationalOperations
     {
+        public static (T Numerator, T Denominator) Add<T, TPolicy>(
+            T leftNumerator, T leftDenominator, T rightNumerator, T rightDenominator, TPolicy policy)
+            where TPolicy : IAdditionFunctions<T>, IDivisionFunctions<T>, IMultiplyFunctions<T>,
+            IGreatestCommonDivisorFunctions<T>
+        {
+            // https://github.com/boostorg/rational/blob/boost-1.90.0/include/boost/rational.hpp#L517
+            var gcd = policy.Gcd(leftDenominator, rightDenominator);
+            leftDenominator = policy.Divide(leftDenominator, gcd);
+            leftNumerator = policy.Add(
+                policy.Multiply(leftNumerator, policy.Divide(rightDenominator, gcd)),
+                policy.Multiply(rightNumerator, leftDenominator));
+            gcd = policy.Gcd(leftNumerator, gcd);
+            var numerator = policy.Divide(leftNumerator, gcd);
+            var denominator = policy.Multiply(leftDenominator, policy.Divide(rightDenominator, gcd));
+            return (numerator, denominator);
+        }
+
         public static (T Numerator, T Denominator) Multiply<T, TPolicy>(
             T leftNumerator, T leftDenominator, T rightNumerator, T rightDenominator, TPolicy policy)
             where TPolicy : IDivisionFunctions<T>, IGreatestCommonDivisorFunctions<T>, IMultiplyFunctions<T>
@@ -12,7 +29,6 @@
             var numerator = policy.Multiply(policy.Divide(leftNumerator, gcd1), policy.Divide(rightNumerator, gcd2));
             var denominator =
                 policy.Multiply(policy.Divide(leftDenominator, gcd2), policy.Divide(rightDenominator, gcd1));
-
             return (numerator, denominator);
         }
     }
