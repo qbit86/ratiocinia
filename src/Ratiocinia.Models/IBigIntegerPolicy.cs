0061d10bf3c9386747ebb04c5b5007e78b1e3a89
Viktor Tselov <qbit86@gmail.com>
2026-01-02 00:46:28 +0200

    [Models] Refactor IBigIntegerPolicy interface and remove ICheckedNumberPolicy; add INumberCheckedPolicy for checked arithmetic

--- src/Ratiocinia.Models/IBigIntegerPolicy.cs
+++ src/Ratiocinia.Models/IBigIntegerPolicy.cs
@@ -3,19 +3,11 @@ namespace Ratiocinia.Models
     using System.Numerics;
 
     public interface IBigIntegerPolicy :
-        IAdditionFunctions<BigInteger>,
-        IDivisionFunctions<BigInteger>,
-        IGreatestCommonDivisorFunctions<BigInteger>,
-        IMultiplyFunctions<BigInteger>
+        IPartialNumberPolicy<BigInteger>,
+        IGreatestCommonDivisorFunctions<BigInteger>
     {
-        BigInteger IAdditionFunctions<BigInteger>.Add(BigInteger left, BigInteger right) => left + right;
-
-        BigInteger IDivisionFunctions<BigInteger>.Divide(BigInteger left, BigInteger right) => left / right;
-
         BigInteger IGreatestCommonDivisorFunctions<BigInteger>.Gcd(BigInteger left, BigInteger right) =>
             BigInteger.GreatestCommonDivisor(left, right);
-
-        BigInteger IMultiplyFunctions<BigInteger>.Multiply(BigInteger left, BigInteger right) => left * right;
     }
 
     public sealed class BigIntegerPolicy : IBigIntegerPolicy
