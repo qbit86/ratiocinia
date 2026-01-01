4098580aefaa80c88a59319903f19c5380e00359
Viktor Tselov <qbit86@gmail.com>
2026-01-01 04:27:15 +0200

    [Models] Add ICheckedNumberPolicy interface and CheckedNumberPolicy class for checked arithmetic operations

---
+++ src/Ratiocinia.Models/ICheckedNumberPolicy.cs
@@ -0,0 +1,18 @@
+﻿namespace Ratiocinia.Models
+{
+    using System.Numerics;
+
+    public interface ICheckedNumberPolicy<T> : IAdditionFunctions<T>, IMultiplyFunctions<T>
+        where T : IAdditionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+    {
+        T IAdditionFunctions<T>.Add(T left, T right) => checked(left + right);
+
+        T IMultiplyFunctions<T>.Multiply(T left, T right) => checked(left * right);
+    }
+
+    public sealed class CheckedNumberPolicy<T> : ICheckedNumberPolicy<T>
+        where T : IAdditionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+    {
+        public static CheckedNumberPolicy<T> Instance { get; } = new();
+    }
+}
