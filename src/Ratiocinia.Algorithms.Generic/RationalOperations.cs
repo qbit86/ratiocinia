917b5b4780f64cdea65e4b8466e9f1aa39272a9a
Viktor Tselov <qbit86@gmail.com>
2026-01-03 02:31:43 +0200

    [Algorithms] Implement Divide and Subtract methods in RationalOperations

--- src/Ratiocinia.Algorithms.Generic/RationalOperations.cs
+++ src/Ratiocinia.Algorithms.Generic/RationalOperations.cs
@@ -1,5 +1,8 @@
 ﻿namespace Ratiocinia.Algorithms.Generic
 {
+    using System;
+    using System.Collections.Generic;
+
     public static class RationalOperations
     {
         public static (T Numerator, T Denominator) Add<T, TPolicy>(
@@ -7,8 +10,8 @@
             where TPolicy :
             IAdditionFunctions<T>,
             IDivisionFunctions<T>,
-            IMultiplyFunctions<T>,
-            IGreatestCommonDivisorFunctions<T>
+            IGreatestCommonDivisorFunctions<T>,
+            IMultiplyFunctions<T>
         {
             // https://github.com/boostorg/rational/blob/boost-1.90.0/include/boost/rational.hpp#L517
             var gcd = policy.Gcd(leftDenominator, rightDenominator);
@@ -22,9 +25,42 @@
             return (numerator, denominator);
         }
 
+        public static (T Numerator, T Denominator) Divide<T, TPolicy>(
+            T leftNumerator, T leftDenominator, T rightNumerator, T rightDenominator, TPolicy policy)
+            where TPolicy :
+            IAdditiveIdentity<T>,
+            IComparer<T>,
+            IDivisionFunctions<T>,
+            IGreatestCommonDivisorFunctions<T>,
+            IMultiplyFunctions<T>,
+            IUnaryNegationFunctions<T>
+        {
+            // https://github.com/boostorg/rational/blob/boost-1.90.0/include/boost/rational.hpp#L586
+            if (policy.Compare(rightNumerator, policy.AdditiveIdentity) is 0)
+                throw new ArgumentOutOfRangeException(nameof(rightNumerator));
+            if (policy.Compare(leftNumerator, policy.AdditiveIdentity) is 0)
+                return (leftNumerator, leftDenominator);
+
+            var gcd1 = policy.Gcd(leftNumerator, rightNumerator);
+            var gcd2 = policy.Gcd(leftDenominator, rightDenominator);
+            var numerator = policy.Multiply(policy.Divide(leftNumerator, gcd1), policy.Divide(rightDenominator, gcd2));
+            var denominator =
+                policy.Multiply(policy.Divide(leftDenominator, gcd2), policy.Divide(rightNumerator, gcd1));
+            if (policy.Compare(denominator, policy.AdditiveIdentity) < 0)
+            {
+                numerator = policy.Negate(numerator);
+                denominator = policy.Negate(denominator);
+            }
+
+            return (numerator, denominator);
+        }
+
         public static (T Numerator, T Denominator) Multiply<T, TPolicy>(
             T leftNumerator, T leftDenominator, T rightNumerator, T rightDenominator, TPolicy policy)
-            where TPolicy : IDivisionFunctions<T>, IGreatestCommonDivisorFunctions<T>, IMultiplyFunctions<T>
+            where TPolicy :
+            IDivisionFunctions<T>,
+            IGreatestCommonDivisorFunctions<T>,
+            IMultiplyFunctions<T>
         {
             // https://github.com/boostorg/rational/blob/boost-1.90.0/include/boost/rational.hpp#L571
             var gcd1 = policy.Gcd(leftNumerator, rightDenominator);
@@ -32,6 +68,26 @@
             var numerator = policy.Multiply(policy.Divide(leftNumerator, gcd1), policy.Divide(rightNumerator, gcd2));
             var denominator =
                 policy.Multiply(policy.Divide(leftDenominator, gcd2), policy.Divide(rightDenominator, gcd1));
+            return (numerator, denominator);
+        }
+
+        public static (T Numerator, T Denominator) Subtract<T, TPolicy>(
+            T leftNumerator, T leftDenominator, T rightNumerator, T rightDenominator, TPolicy policy)
+            where TPolicy :
+            IDivisionFunctions<T>,
+            IGreatestCommonDivisorFunctions<T>,
+            IMultiplyFunctions<T>,
+            ISubtractionFunctions<T>
+        {
+            // https://github.com/boostorg/rational/blob/boost-1.90.0/include/boost/rational.hpp#L552
+            var gcd = policy.Gcd(leftDenominator, rightDenominator);
+            leftDenominator = policy.Divide(leftDenominator, gcd);
+            leftNumerator = policy.Subtract(
+                policy.Multiply(leftNumerator, policy.Divide(rightDenominator, gcd)),
+                policy.Multiply(rightNumerator, leftDenominator));
+            gcd = policy.Gcd(leftNumerator, gcd);
+            var numerator = policy.Divide(leftNumerator, gcd);
+            var denominator = policy.Multiply(leftDenominator, policy.Divide(rightDenominator, gcd));
             return (numerator, denominator);
         }
     }
