1e1415ad92b8d400f84f183f3848c07ba574fe63
Viktor Tselov <qbit86@gmail.com>
2026-01-05 03:52:03 +0200

    [Models] Update IAdditiveIdentityComparer to implement IEquatable interface

--- src/Ratiocinia.Models/IAdditiveIdentityComparer.cs
+++ src/Ratiocinia.Models/IAdditiveIdentityComparer.cs
@@ -3,9 +3,11 @@ namespace Ratiocinia.Models
     using System;
     using System.Numerics;
 
-    public interface IAdditiveIdentityComparer<in T> : IComparable<T>
+    public interface IAdditiveIdentityComparer<T> : IComparable<T>, IEquatable<T>
         where T : IAdditiveIdentity<T, T>, IComparable<T>
     {
         int IComparable<T>.CompareTo(T? other) => T.AdditiveIdentity.CompareTo(other);
+
+        bool IEquatable<T>.Equals(T? other) => T.AdditiveIdentity.CompareTo(other) is 0;
     }
 }
