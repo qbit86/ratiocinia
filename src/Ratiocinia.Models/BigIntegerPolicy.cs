aa64ac60b10542a98e1bdd474fd5b8b2a2ee4f8e
Viktor Tselov <qbit86@gmail.com>
2026-01-06 21:36:27 +0200

    [Models] Refactor BigIntegerPolicy to implement IComparer<BigInteger>

--- src/Ratiocinia.Models/BigIntegerPolicy.cs
+++ src/Ratiocinia.Models/BigIntegerPolicy.cs
@@ -1,18 +1,23 @@
 namespace Ratiocinia.Models
 {
+    using System.Collections.Generic;
     using System.Numerics;
 
     public sealed class BigIntegerPolicy :
+        IComparer<BigInteger>,
         IGreatestCommonDivisorFunctions<BigInteger>,
         INumberBaseAbsoluteFunctions<BigInteger>,
         INumericModulusFunctions<BigInteger>,
         IUncheckedAdditionFunctions<BigInteger>,
+        IUncheckedDecrementFunctions<BigInteger>,
         IUncheckedDivisionFunctions<BigInteger>,
         IUncheckedSubtractionFunctions<BigInteger>,
         IUncheckedMultiplyFunctions<BigInteger>,
         IUncheckedUnaryNegationFunctions<BigInteger>
     {
         public static BigIntegerPolicy Instance { get; } = new();
+
+        public int Compare(BigInteger x, BigInteger y) => x.CompareTo(y);
 
         public BigInteger Gcd(BigInteger left, BigInteger right) => BigInteger.GreatestCommonDivisor(left, right);
     }
