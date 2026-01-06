171a15c004192508ab221cbc7ee58fb39c5ff6d3
Viktor Tselov <qbit86@gmail.com>
2026-01-06 14:04:09 +0200

    [Algorithms] Add NumericRationalOperations class with IsNormalized method

---
+++ src/Ratiocinia.Algorithms.Specialized/NumericRationalOperations.cs
@@ -0,0 +1,22 @@
+namespace Ratiocinia.Algorithms.Specialized
+{
+    using System;
+    using System.Numerics;
+    using Generic;
+    using Models;
+
+    public static class NumericRationalOperations
+    {
+        public static bool IsNormalized<T>(T numerator, T denominator)
+            where T : IAdditiveIdentity<T, T>, IComparable<T>, IEquatable<T>, IModulusOperators<T, T, T>,
+            INumberBase<T>, IMultiplicativeIdentity<T, T>
+        {
+            Policy<T> policy = default;
+            return RationalOperations.IsNormalized(
+                numerator, denominator, T.AdditiveIdentity, T.MultiplicativeIdentity, policy);
+        }
+    }
+
+    file readonly struct Policy<T> : INumberBaseGreatestCommonDivisorFunctions<T>, INumberBaseAbsoluteFunctions<T>
+        where T : IModulusOperators<T, T, T>, INumberBase<T>;
+}
