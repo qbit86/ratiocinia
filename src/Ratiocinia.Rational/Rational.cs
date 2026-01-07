d313e2f47c0c35b2f1b66f667f6406045c69a486
Viktor Tselov <qbit86@gmail.com>
2026-01-07 02:24:33 +0200

    [Rational] Refactor Rational<T> struct: remove unused interfaces and replace static additive identity reference

--- src/Ratiocinia.Rational/Rational.cs
+++ src/Ratiocinia.Rational/Rational.cs
@@ -7,15 +7,10 @@
 
     public readonly partial struct Rational<T>
         where T :
-        IAdditiveIdentity<T, T>,
         IComparable<T>,
-        IEquatable<T>,
         IModulusOperators<T, T, T>,
-        IMultiplicativeIdentity<T, T>,
         INumberBase<T>
     {
-        private static readonly Rational<T> s_additiveIdentity = new(T.AdditiveIdentity, T.MultiplicativeIdentity);
-
         private Rational(T numerator, T denominator) => (Numerator, Denominator) = (numerator, denominator);
 
         public T Numerator { get; }
@@ -27,11 +22,16 @@
         public static bool TryCreate(T numerator, T denominator, out Rational<T> rational)
         {
             bool result = NumericRationalOperations.IsNormalized(numerator, denominator);
-            rational = result ? new(numerator, denominator) : s_additiveIdentity;
+            rational = result ? new(numerator, denominator) : AdditiveIdentity;
             return result;
         }
 
-        public static Rational<T> Create(T numerator, T denominator) => throw new NotImplementedException();
+        public static Rational<T> Create(T numerator, T denominator)
+        {
+            var (normalizedNumerator, normalizedDenominator) =
+                CheckedRationalOperations.Normalize(numerator, denominator);
+            return new(normalizedNumerator, normalizedDenominator);
+        }
 
         public override string ToString() => ToString(string.Empty, CultureInfo.InvariantCulture);
     }
