180a23825b473bca8f6b3a471bf9e21660fff8cc
Viktor Tselov <qbit86@gmail.com>
2026-01-07 09:53:55 +0200

    [Rational] Refactor Parse method in Rational<T> struct to simplify error handling

--- src/Ratiocinia.Rational/Rational.ISpanParsable.cs
+++ src/Ratiocinia.Rational/Rational.ISpanParsable.cs
@@ -13,9 +13,7 @@ namespace Ratiocinia
             TryParse(s.AsSpan(), provider, out result);
 
         public static Rational<T> Parse(ReadOnlySpan<char> s, IFormatProvider? provider) =>
-            TryParse(s, provider, out var result)
-                ? result
-                : throw new FormatException("The input string was not in a correct format.");
+            Parse(s, NumberStyles.None, provider);
 
         public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, out Rational<T> result) =>
             TryParseCore(s, NumberStyles.None, provider, out result);
