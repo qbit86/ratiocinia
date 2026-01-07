8bfa2177c7971f47096cc2a557a9ac93c52f833d
Viktor Tselov <qbit86@gmail.com>
2026-01-07 03:56:27 +0200

    [Rational] Refactor TryParse method in Rational<T> struct to streamline parsing logic

--- src/Ratiocinia.Rational/Rational.ISpanParsable.cs
+++ src/Ratiocinia.Rational/Rational.ISpanParsable.cs
@@ -2,6 +2,7 @@ namespace Ratiocinia
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
 
     partial struct Rational<T> : ISpanParsable<Rational<T>>
     {
@@ -17,59 +18,6 @@ namespace Ratiocinia
                 : throw new FormatException("The input string was not in a correct format.");
 
         public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, out Rational<T> result) =>
-            TryParseCore(s, provider, out result);
-
-        private static bool TryParseCore(ReadOnlySpan<char> s, IFormatProvider? provider, out Rational<T> result)
-        {
-            s = s.Trim();
-            if (s.IsEmpty)
-                return None(out result);
-
-            int slashIndex = s.IndexOf('/');
-            if (slashIndex < 0)
-            {
-                if (!T.TryParse(s, provider, out var numerator))
-                    return None(out result);
-
-                result = Create(numerator, T.MultiplicativeIdentity);
-                return true;
-            }
-
-            // Reject more than one separator
-            if (s[(slashIndex + 1)..].IndexOf('/') >= 0)
-                return None(out result);
-
-            var numeratorSpan = s[..slashIndex].Trim();
-            var denominatorSpan = s[(slashIndex + 1)..].Trim();
-            if (numeratorSpan.IsEmpty || denominatorSpan.IsEmpty)
-                return None(out result);
-
-            if (!T.TryParse(numeratorSpan, provider, out var parsedNumerator) ||
-                !T.TryParse(denominatorSpan, provider, out var parsedDenominator))
-                return None(out result);
-
-            if (parsedDenominator.Equals(T.AdditiveIdentity))
-                return None(out result);
-
-            try
-            {
-                result = Create(parsedNumerator, parsedDenominator);
-                return true;
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-                return None(out result);
-            }
-            catch (OverflowException)
-            {
-                return None(out result);
-            }
-
-            static bool None(out Rational<T> r)
-            {
-                r = AdditiveIdentity;
-                return false;
-            }
-        }
+            TryParseCore(s, NumberStyles.None, provider, out result);
     }
 }
