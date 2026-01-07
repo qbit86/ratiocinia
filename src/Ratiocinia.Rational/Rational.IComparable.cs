66f4654822dd8ec1849d0182f3810f446f28cc66
Viktor Tselov <qbit86@gmail.com>
2026-01-07 03:55:46 +0200

    [Rational] Implement IComparable interface for Rational<T> struct

---
+++ src/Ratiocinia.Rational/Rational.IComparable.cs
@@ -0,0 +1,27 @@
+namespace Ratiocinia
+{
+    using System;
+    using System.Diagnostics;
+    using Algorithms.Specialized;
+
+    partial struct Rational<T> : IComparable<Rational<T>>
+    {
+        public int CompareTo(Rational<T> other)
+        {
+            // NaN compares greater than any non-NaN
+            if (Equals(other))
+                return 0;
+            if (T.IsZero(Denominator))
+                return 1;
+            if (T.IsZero(other.Denominator))
+                return -1;
+
+            if (UncheckedRationalOperations.LessThan(Numerator, Denominator, other.Numerator, other.Denominator))
+                return -1;
+
+            Debug.Assert(
+                UncheckedRationalOperations.LessThan(other.Numerator, other.Denominator, Numerator, Denominator));
+            return 1;
+        }
+    }
+}
