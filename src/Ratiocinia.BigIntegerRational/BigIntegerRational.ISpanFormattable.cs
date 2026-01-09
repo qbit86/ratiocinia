e909c4c599f52b217f44a8da90bf8cba2053e406
Viktor Tselov <qbit86@gmail.com>
2026-01-09 02:57:28 +0200

    [Rational] Refactor ToString methods in BigIntegerRational and Rational<T> to use expression-bodied members

--- src/Ratiocinia.BigIntegerRational/BigIntegerRational.ISpanFormattable.cs
+++ src/Ratiocinia.BigIntegerRational/BigIntegerRational.ISpanFormattable.cs
@@ -4,11 +4,8 @@ namespace Ratiocinia
 
     partial struct BigIntegerRational
     {
-        public string ToString(string? format, IFormatProvider? formatProvider)
-        {
-            FormattableString formattable = $"{Numerator}/{Denominator}";
-            return formattable.ToString(formatProvider);
-        }
+        public string ToString(string? format, IFormatProvider? formatProvider) =>
+            $"{Numerator.ToString(format, formatProvider)}/{Denominator.ToString(format, formatProvider)}";
 
         public bool TryFormat(
             Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider) =>
