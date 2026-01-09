05f10351cf716377a4e55fc09c311bf670548630
Viktor Tselov <qbit86@gmail.com>
2026-01-09 03:00:52 +0200

    [Rational] Refactor TryFormat methods in BigIntegerRational and Rational<T> to improve formatting logic

--- src/Ratiocinia.Rational/Rational.ISpanFormattable.cs
+++ src/Ratiocinia.Rational/Rational.ISpanFormattable.cs
@@ -8,7 +8,25 @@ namespace Ratiocinia
             $"{Numerator.ToString(format, formatProvider)}/{Denominator.ToString(format, formatProvider)}";
 
         public bool TryFormat(
-            Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider) =>
-            destination.TryWrite(provider, $"{Numerator}/{Denominator}", out charsWritten);
+            Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider)
+        {
+            charsWritten = 0;
+
+            if (!Numerator.TryFormat(destination, out int numeratorCharsWritten, format, provider))
+                return false;
+
+            int offset = numeratorCharsWritten;
+            if (offset >= destination.Length)
+                return false;
+
+            destination[offset] = '/';
+            offset++;
+
+            if (!Denominator.TryFormat(destination[offset..], out int denominatorCharsWritten, format, provider))
+                return false;
+
+            charsWritten = offset + denominatorCharsWritten;
+            return true;
+        }
     }
 }
