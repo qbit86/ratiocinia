14756f5e147ea4963817efc28d52985248aa3245
Viktor Tselov <qbit86@gmail.com>
2026-01-15 03:58:05 +0200

    [Tests] Add tests for Reciprocal method in Checked and Unchecked RationalOperations

--- tests/Tests.Algorithms/CheckedAndUncheckedRationalOperationsTests.cs
+++ tests/Tests.Algorithms/CheckedAndUncheckedRationalOperationsTests.cs
@@ -156,4 +156,62 @@ public sealed class CheckedAndUncheckedRationalOperationsTests
         Assert.Equal(-1, n);
         Assert.Equal(1, d);
     }
+
+    [Fact]
+    public void Checked_Reciprocal_returns_swapped_numerator_and_denominator()
+    {
+        (int n, int d) = CheckedRationalOperations.Reciprocal(2, 3);
+        Assert.Equal(3, n);
+        Assert.Equal(2, d);
+    }
+
+    [Fact]
+    public void Unchecked_Reciprocal_returns_swapped_numerator_and_denominator()
+    {
+        (int n, int d) = UncheckedRationalOperations.Reciprocal(2, 3);
+        Assert.Equal(3, n);
+        Assert.Equal(2, d);
+    }
+
+    [Fact]
+    public void Checked_Reciprocal_with_negative_numerator_normalizes_sign()
+    {
+        (int n, int d) = CheckedRationalOperations.Reciprocal(-2, 3);
+        Assert.Equal(-3, n);
+        Assert.Equal(2, d);
+    }
+
+    [Fact]
+    public void Unchecked_Reciprocal_with_negative_numerator_normalizes_sign()
+    {
+        (int n, int d) = UncheckedRationalOperations.Reciprocal(-2, 3);
+        Assert.Equal(-3, n);
+        Assert.Equal(2, d);
+    }
+
+    [Fact]
+    public void Checked_Reciprocal_with_numerator_zero_throws()
+    {
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => CheckedRationalOperations.Reciprocal(0, 3));
+        Assert.Equal("numerator", ex.ParamName);
+    }
+
+    [Fact]
+    public void Unchecked_Reciprocal_with_numerator_zero_throws()
+    {
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => UncheckedRationalOperations.Reciprocal(0, 3));
+        Assert.Equal("numerator", ex.ParamName);
+    }
+
+    [Fact]
+    public void Checked_Reciprocal_overflow_throws()
+        => Assert.Throws<OverflowException>(() => CheckedRationalOperations.Reciprocal(int.MinValue, 1));
+
+    [Fact]
+    public void Unchecked_Reciprocal_overflow_wraps()
+    {
+        (int n, int d) = UncheckedRationalOperations.Reciprocal(int.MinValue, 1);
+        Assert.Equal(-1, n);
+        Assert.Equal(int.MinValue, d);
+    }
 }
